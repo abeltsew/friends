9a4c787aa3734b532fa1fae60ad1fccceb0d6f6e
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-03-10 20:55:12 +0300

    Add guest section

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,88 @@ const setBg = (on) => {
 window.addEventListener('scroll', () =>
   window.scrollY > 60 ? setBg(true) : setBg()
 );
+
+const guests = [
+  {
+    name: 'Chandler Bing',
+    title: 'An Executive in statistical analysis and data reconfiguration',
+    description:
+      'Chandler is Known for his sarcastic sense of humor and bad luck in relationship.',
+    image: '/assets/images/cb.jpeg',
+  },
+  {
+    name: 'Rachel Green',
+    title:
+      "waitress at the coffee house Central Perk and Assitant in Bloomingdale's",
+    description:
+      "A fashion enthusiast and Monica Geller's best friend from childhood and Ross's big crash.",
+    image: '/assets/images/rgreen.jpeg',
+  },
+  {
+    name: 'Monica Geller',
+    title: 'The "mother hen" of the group and a chef and a neat freak.',
+    description:
+      'Monica is known for her perfectionist, bossy, competitive, and obsessive-compulsive nature.',
+    image: '/assets/images/mg.jpeg',
+  },
+  {
+    name: 'Phoebe Buffay',
+    title: 'A masseuse and self-taught musician and author of "smally-cat"',
+    description:
+      'She writes and sings her own strange songs, accompanying herself on the guitar.',
+    image: '/assets/images/pb.jpeg',
+  },
+  {
+    name: 'Joey Tribbiani',
+    title: 'A struggling famous actor for his role on soap opera.',
+    description:
+      'Joey has many short-term girlfriends. Despite his womanizing, Joey is innocent, caring, and well-intentioned',
+    image: '/assets/images/jt.jpeg',
+  },
+  {
+    name: 'Ross Geller',
+    title: 'A palaeontologist working at the Museum of Natural History',
+    description:
+      'Ross is involved in an on-again, off-again relationship with Rachel throughout the series.',
+    image: '/assets/images/rg.jpeg',
+  },
+];
+
+const guestsList = document.querySelector('.guests-list');
+
+guests.forEach((guest) => {
+  const guestDiv = document.createElement('div');
+  guestDiv.classList.add('guest-div');
+  const img = document.createElement('img');
+  img.classList.add('guest-img');
+  img.src = guest.image;
+
+  const imgCover = document.createElement('img');
+  imgCover.classList.add('img-cover');
+  imgCover.src = '/assets/images/bg-art.png';
+
+  const guestDescription = document.createElement('div');
+  guestDescription.classList.add('guest-description');
+
+  const guestName = document.createElement('h3');
+  guestName.classList.add('guest-name');
+  guestName.innerHTML = guest.name;
+
+  const guestTitle = document.createElement('h3');
+  guestTitle.classList.add('guest-title');
+  guestTitle.innerHTML = guest.title;
+
+  const guestDesc = document.createElement('h3');
+  guestDesc.classList.add('guest-desc');
+  guestDesc.innerHTML = guest.description;
+
+  guestDescription.appendChild(guestName);
+  guestDescription.appendChild(guestTitle);
+  guestDescription.appendChild(guestDesc);
+
+  guestDiv.appendChild(imgCover);
+  guestDiv.appendChild(img);
+  guestDiv.appendChild(guestDescription);
+
+  guestsList.appendChild(guestDiv);
+});
